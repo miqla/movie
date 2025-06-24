e45881994cf71c4a7131aaf169f331e0df1e54ec
Miqdad <miqdad.shaldin@gmail.com>
2025-06-24 22:29:31 +0700

    create base url for tmdb api

--- movies/src/api/movies.js
+++ movies/src/api/movies.js
@@ -19,3 +19,12 @@ export const queryMovies = (dynamicURL) => {
   });
   return customURL;
 };
+
+export const tmdbApi = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4YzY2Zjc0NGRkNGQ2MjljYWEwN2UzZWU0YzAxODQ3ZCIsIm5iZiI6MTc1MDUxNzM4Ny42NCwic3ViIjoiNjg1NmM2OGIzZGM0MDFjZDFmZWM1OTYzIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.j-VuY5M6oH6BxqdpZQR_a-B-oiafX9v4rpCpqWHpuYM",
+  },
+});
