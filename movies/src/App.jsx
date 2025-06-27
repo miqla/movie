14883c5a8d2d289f2ab3c7e9c2152f1a1716f222
Miqdad <miqdad.shaldin@gmail.com>
2025-06-28 00:23:19 +0700

    change browser router to hashrouter

--- movies/src/App.jsx
+++ movies/src/App.jsx
@@ -1,16 +1,16 @@
 import "./App.css";
 import { Movie } from "./pages/Movie";
 import { Favorite } from "./pages/Favorite";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { Routes, Route, HashRouter } from "react-router";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Movie />} />
         <Route path="/favorite" element={<Favorite />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
